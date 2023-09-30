3cace5bff3c069fa04bed21118dabf1e67fa9d05
Vaughn-Man <vaughnpardillo@gmail.com>
2023-09-30 12:16:30 +0800

    update

--- script.js
+++ script.js
@@ -1,14 +1,253 @@
-function myFunction() {
-    var table = document.getElementById('gradetable');
-    var row = table.insertRow(2);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    cell1.innerHTML = document.getElementById('tname').value;
-    cell2.innerHTML = document.getElementById('tage').value;
-    cell3.innerHTML = document.getElementById('tsection').value;
-}
-function deleteRow() {
+var crudApp = new function () {
 
-    document.getElementById("gradetable").deleteRow(0);
-  }
+
+    this.flights = [
+        { ID: '1', Client_Name: 'Jhon Doe', Country: 'USA', Fare: 125.60 },
+        { ID: '2', Client_Name: 'Wes Smith', Country: 'Spain', Fare: 56.00 },
+        { ID: '3', Client_Name: 'Mary Ann', Country: 'Italy', Fare: 210.40 }
+    ]
+
+    this.country = ['USA', 'UK', 'Germany', 'France', 'Mexico', 'Spain', 'Italy', 'China', 'Russia'];
+    this.col = [];
+
+
+
+    this.createTable = function () {
+
+
+        for (var i = 0; i < this.flights.length; i++) {
+            for (var key in this.flights[i]) {
+                if (this.col.indexOf(key) === -1) {
+                    this.col.push(key);
+                }
+            }
+        }
+
+
+        var table = document.createElement('table');
+        table.setAttribute('id', 'booksTable');
+
+        var tr = table.insertRow(-1);
+
+        for (var h = 0; h < this.col.length; h++) {
+
+            var th = document.createElement('th');
+            th.innerHTML = this.col[h].replace('_', ' ');
+            tr.appendChild(th);
+        }
+
+
+        for (var i = 0; i < this.flights.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (var j = 0; j < this.col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = this.flights[i][this.col[j]];
+            }
+
+
+            this.td = document.createElement('td');
+
+
+            tr.appendChild(this.td);
+            var lblCancel = document.createElement('label');
+            lblCancel.innerHTML = '✖';
+            lblCancel.setAttribute('onclick', 'crudApp.Cancel(this)');
+            lblCancel.setAttribute('style', 'display:none;');
+            lblCancel.setAttribute('title', 'Cancel');
+            lblCancel.setAttribute('id', 'lbl' + i);
+            this.td.appendChild(lblCancel);
+
+
+            tr.appendChild(this.td);
+            var btSave = document.createElement('input');
+
+            btSave.setAttribute('type', 'button');
+            btSave.setAttribute('value', 'Save');
+            btSave.setAttribute('id', 'Save' + i);
+            btSave.setAttribute('style', 'display:none;');
+            btSave.setAttribute('onclick', 'crudApp.Save(this)');
+            this.td.appendChild(btSave);
+
+            tr.appendChild(this.td);
+            var btUpdate = document.createElement('input');
+
+            btUpdate.setAttribute('type', 'button');
+            btUpdate.setAttribute('value', 'Update');
+            btUpdate.setAttribute('id', 'Edit' + i);
+            btUpdate.setAttribute('style', 'background-color:#44CCEB;');
+            btUpdate.setAttribute('onclick', 'crudApp.Update(this)');
+            this.td.appendChild(btUpdate);
+
+            this.td = document.createElement('th');
+            tr.appendChild(this.td);
+            var btDelete = document.createElement('input');
+            btDelete.setAttribute('type', 'button');
+            btDelete.setAttribute('value', 'Delete');
+            btDelete.setAttribute('style', 'background-color:#ED5650;');
+            btDelete.setAttribute('onclick', 'crudApp.Delete(this)');
+            this.td.appendChild(btDelete);
+        }
+
+
+
+        tr = table.insertRow(-1);
+
+        for (var j = 0; j < this.col.length; j++) {
+            var newCell = tr.insertCell(-1);
+            if (j >= 1) {
+
+                if (j == 2) {
+
+                    var select = document.createElement('select');
+                    select.innerHTML = '<option value=""></option>';
+                    for (k = 0; k < this.country.length; k++) {
+                        select.innerHTML = select.innerHTML +
+                            '<option value="' + this.country[k] + '">' + this.country[k] + '</option>';
+                    }
+                    newCell.appendChild(select);
+                }
+                else {
+                    var tBox = document.createElement('input');
+                    tBox.setAttribute('type', 'text');
+                    tBox.setAttribute('value', '');
+                    newCell.appendChild(tBox);
+                }
+            }
+        }
+
+        this.td = document.createElement('td');
+        tr.appendChild(this.td);
+
+        var btNew = document.createElement('input');
+
+        btNew.setAttribute('type', 'button');
+        btNew.setAttribute('value', 'Create');
+        btNew.setAttribute('id', 'New' + i);
+        btNew.setAttribute('style', 'background-color:#207DD1;');
+        btNew.setAttribute('onclick', 'crudApp.CreateNew(this)');
+        this.td.appendChild(btNew);
+
+        var div = document.getElementById('container');
+        div.innerHTML = '';
+        div.appendChild(table);
+    };
+
+
+    this.Cancel = function (oButton) {
+
+        oButton.setAttribute('style', 'display:none; float:none;');
+
+        var activeRow = oButton.parentNode.parentNode.rowIndex;
+
+
+        var btSave = document.getElementById('Save' + (activeRow - 1));
+        btSave.setAttribute('style', 'display:none;');
+
+
+        var btUpdate = document.getElementById('Edit' + (activeRow - 1));
+        btUpdate.setAttribute('style', 'display:block; margin:0 auto; background-color:#44CCEB;');
+
+        var tab = document.getElementById('booksTable').rows[activeRow];
+
+        for (i = 0; i < this.col.length; i++) {
+            var td = tab.getElementsByTagName("td")[i];
+            td.innerHTML = this.flights[(activeRow - 1)][this.col[i]];
+        }
+    }
+
+
+    this.Update = function (oButton) {
+        var activeRow = oButton.parentNode.parentNode.rowIndex;
+        var tab = document.getElementById('booksTable').rows[activeRow];
+
+
+        for (i = 1; i < 4; i++) {
+            if (i == 2) {
+                var td = tab.getElementsByTagName("td")[i];
+                var ele = document.createElement('select');
+                ele.innerHTML = '<option value="' + td.innerText + '">' + td.innerText + '</option>';
+                for (k = 0; k < this.country.length; k++) {
+                    ele.innerHTML = ele.innerHTML +
+                        '<option value="' + this.country[k] + '">' + this.country[k] + '</option>';
+                }
+                td.innerText = '';
+                td.appendChild(ele);
+            }
+            else {
+                var td = tab.getElementsByTagName("td")[i];
+                var ele = document.createElement('input');
+                ele.setAttribute('type', 'text');
+                ele.setAttribute('value', td.innerText);
+                td.innerText = '';
+                td.appendChild(ele);
+            }
+        }
+
+        var lblCancel = document.getElementById('lbl' + (activeRow - 1));
+        lblCancel.setAttribute('style', 'cursor:pointer; display:block; width:20px; float:left; position: absolute;');
+
+        var btSave = document.getElementById('Save' + (activeRow - 1));
+        btSave.setAttribute('style', 'display:block; margin-left:30px; float:left; background-color:#2DBF64;');
+
+
+        oButton.setAttribute('style', 'display:none;');
+    };
+
+
+
+    this.Delete = function (oButton) {
+        var activeRow = oButton.parentNode.parentNode.rowIndex;
+        this.flights.splice((activeRow - 1), 1);
+        this.createTable();
+    };
+
+
+    this.Save = function (oButton) {
+        var activeRow = oButton.parentNode.parentNode.rowIndex;
+        var tab = document.getElementById('booksTable').rows[activeRow];
+
+
+        for (i = 1; i < this.col.length; i++) {
+            var td = tab.getElementsByTagName("td")[i];
+            if (td.childNodes[0].getAttribute('type') == 'text' || td.childNodes[0].tagName == 'SELECT') {
+                this.flights[(activeRow - 1)][this.col[i]] = td.childNodes[0].value;
+            }
+        }
+        this.createTable();
+    }
+
+
+    this.CreateNew = function (oButton) {
+        var activeRow = oButton.parentNode.parentNode.rowIndex;
+        var tab = document.getElementById('booksTable').rows[activeRow];
+        var obj = {};
+
+
+        for (i = 1; i < this.col.length; i++) {
+            var td = tab.getElementsByTagName("td")[i];
+            if (td.childNodes[0].getAttribute('type') == 'text' || td.childNodes[0].tagName == 'SELECT') {
+                var txtVal = td.childNodes[0].value;
+                if (txtVal != '') {
+                    obj[this.col[i]] = txtVal.trim();
+                }
+                else {
+                    obj = '';
+                    alert('all fields are compulsory');
+                    break;
+                }
+            }
+        }
+        obj[this.col[0]] = this.flights.length + 1;
+
+        if (Object.keys(obj).length > 0) {
+            this.flights.push(obj);
+            this.createTable();
+        }
+    }
+
+
+}
+
+crudApp.createTable();
